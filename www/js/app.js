8f0b980036486d44bc757329ddba0b9fa0f6218e
xuan9 <xuan@XuandeMacBook-Pro.local>
2015-03-21 23:56:51 +0100

    initial

---
+++ www/js/app.js
@@ -0,0 +1,113 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.services' is found in services.js
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'ngCordova', 'starter.controllers', 'starter.services'])
+
+.run(function($ionicPlatform,$ionicGesture,$rootScope,$timeout,$cordovaSQLite,Fav,Cat,Files,FavTask) {
+  $ionicPlatform.ready(function() {
+    console.log('ioni ready, init app...');
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+    // Fav.clean();Cat.clean(); Files.clean();
+    Fav.prepare(); Cat.prepare(); Files.prepare();
+
+    // Cat.listFavorites().then(function(favs) {
+    //   for (i = 0; i < favs.length; i++) {
+    //      FavTask.storeFavBook(favs[i].name,favs[i].isFav,true);
+    //   }
+    // }, function(error) {
+    //   console.error('Unable to get favorites', error);
+    // });
+
+  });
+
+  // $rootScope.$on('$routeChangeSuccess', function(newRoute, oldRoute) {
+  //   $location.hash($routeParams.j);
+  //   $anchorScroll();
+  // });
+
+
+})
+
+.config(function($stateProvider, $urlRouterProvider, $uiViewScrollProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+
+    // setup an abstract state for the tabs directive
+    .state('tab', {
+      url: "/tab",
+      abstract: true,
+      templateUrl: "templates/tabs.html"
+    })
+
+    // Each tab has its own nav history stack:
+
+    .state('tab.dash', {
+      url: '/dash',
+      views: {
+        'tab-dash': {
+          templateUrl: 'templates/tab-dash.html',
+          controller: 'DashCtrl'
+        }
+      }
+    })
+
+    .state('tab.friends', {
+      url: '/friends',
+      views: {
+        'tab-friends': {
+          templateUrl: 'templates/tab-friends.html',
+          controller: 'FriendsCtrl'
+        }
+      }
+    })
+    .state('tab.friend-detail', {
+      url: '/friend/:friendId',
+      views: {
+        'tab-friends': {
+          templateUrl: 'templates/friend-detail.html',
+          controller: 'FriendDetailCtrl'
+        }
+      }
+    })
+
+    .state('tab.book', {
+      url: "/book/{name:.+}",
+      views: {
+        'tab-book': {
+          templateUrl: 'templates/tab-book.html',
+          controller: 'BookCtrl'
+        }
+      }
+    });
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/tab/dash');
+  $uiViewScrollProvider.useAnchorScroll();
+
+})
+// .run(function($ionicPlatform, $rootScope, $location, $anchorScroll, $stateParams) {
+// $ionicPlatform.ready(function() {
+//   $rootScope.$on('$routeChangeSuccess', function(newRoute, oldRoute) {
+//     console.log("routeChangeSuccess: ", newRoute, oldRoute);
+//     $location.hash($stateParams.j);
+//     $anchorScroll();
+//   });
+// });
+// })
+;
---
+++ www/js/controllers.js
@@ -0,0 +1,160 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($ionicPlatform, $rootScope, $scope, $state,
+  $rootScope, $timeout, $ionicModal, Utils, Cat, Fav, FavTask, Ctrls) {
+
+
+  console.debug('show library');
+  $scope.libraryBooks = ['禮記', '論語', '莊子', '關尹子', '六祖壇經', '五燈會元',
+    '脂硯齋重評石頭記'
+  ];
+
+  var loadFavorites = function(cb) {
+    Cat.listFavorites().then(function(favs) {
+      $rootScope.favorites = favs;
+      if (cb) cb(favs)
+    });
+  };
+
+  $scope.showLibrary = function() {
+    // $scope.addBook = function(name){
+    //   console.debug('add book: ' + name);
+    //   Fav.save(name).then(function(){
+    //     FavTask.storeFavBook(name,1, true)
+    //     loadFavorites();//refresh favs on added
+    //   });
+    // };
+
+    $scope.toggleFavorite = function(name, isFav) {
+      console.log("toggleFavorite: " + name + ": " + isFav);
+      Ctrls.toggleFavorite($ionicModal, $scope, name, isFav ? 1 : 0,
+        loadFavorites);
+    };
+
+    $scope.isFav = function(name) {
+      for (var i = 0; i < $rootScope.favorites.length; i++) {
+        if ($rootScope.favorites[i].name == name) {
+          return true;
+        }
+      }
+      return false;
+    }
+
+    $scope.isNotLib = function(fav) {
+      return $scope.libraryBooks.indexOf(fav.name) == -1;
+    }
+
+    $ionicModal.fromTemplateUrl('model-library.html', {
+      scope: $scope,
+      animation: 'slide-in-up'
+    }).then(function(modal) {
+      console.debug('model: ' + modal);
+      $scope.libraryModal = modal;
+      modal.show();
+      //Cleanup the modal when we're done with it!
+      $scope.$on('$destroy', function() {
+        if ($scope.libraryModal) {
+          $scope.libraryModal.remove();
+        }
+      });
+    });
+  };
+
+  //show lib if no fav
+  $ionicPlatform.ready(function() {
+    if (!$rootScope.favorites) {
+      loadFavorites(function(favs) {
+        if (!favs || favs.length == 0) {
+          $scope.showLibrary();
+        }
+      });
+    }
+  });
+})
+
+.controller('BookCtrl', function($scope, $rootScope, $state, $ionicPlatform,
+  $ionicGesture, $cordovaFile, $timeout, $stateParams, $sce, $ionicModal,
+  Wikipedia, Utils, Fav, FavTask, Files, Ctrls) {
+  // console.log('Hide tabs on bookCtrl');
+  // Utils.hideTabs(true);
+  // if(!$scope.hideTabEventHandlerRegistered ){
+  //   $ionicGesture.on('doubletap', function(){
+  //       console.log('Show tabs on double tap');
+  //       Utils.hideTabs(false);
+  //       $timeout.cancel($rootScope.tabHideTimer);
+  //       $rootScope.tabHideTimer = $timeout(function (){
+  //         console.log('now auto hide tabs.');
+  //         Utils.hideTabs(true);
+  //       }, 5000);
+  //   }, angular.element(document.getElementById('bookView')));
+  //   $scope.hideTabEventHandlerRegistered = true;
+  // }
+
+  var name = $stateParams.name;
+  console.log('Book Name: ' + name);
+
+  $scope.goHome = function() {
+    $state.go('tab.dash');
+  };
+
+  var getFavBook = function(cb) {
+    Fav.get(name).then(function(fav) {
+      $scope.fav = fav;
+      $scope.favorite = fav ? fav.isFav : 0;
+      console.log("IsFav: " + $scope.favorite);
+      if (cb) cb(fav);
+    });
+  };
+
+  $scope.toggleFavorite = function() {
+    console.log("toggleFavorite: " + name + ": " + $scope.favorite);
+    console.log(Ctrls);
+    console.log(Ctrls.toggleFavorite.toString());
+    Ctrls.toggleFavorite($ionicModal, $scope, name, $scope.favorite,
+      getFavBook);
+    $rootScope.favorites = null;
+  };
+
+  var fetchBook = function() {
+    Wikipedia.search(name).then(function(resp) {
+      var book = resp.parse;
+      if (book && book.text) {
+        var bk = book.title;
+        book.title = bk; //bk.indexOf(':') > 0 ? bk.substring(bk.indexOf(':') + 1) : bk
+        book.text = $sce.trustAsHtml(Wikipedia.format(book.text['*']));
+        $scope.book = book;
+        if ($scope.favorite > 0) {
+          //save the missing favorite book
+          Files.saver(name, book.title, book.text);
+        }
+      } else {
+        console.error("not found: " + resp);
+        setTimeout(function() {
+          window.history.back();
+        }, 100);
+      }
+    }, function(error) {
+      console.error('Unable to get Book: ' + +error.code + ": " +
+        error.message);
+    });
+  };
+
+  $ionicPlatform.ready(function() {
+    //check if favorite
+    getFavBook(function(fav) {
+      if (fav) {
+        //try read from file sys
+        Files.get(fav.name).then(function(book) {
+          $scope.book = book;
+        }, function(err) {
+          //if could not find in file sys, load from web
+          fetchBook();
+        });
+      } else {
+        fetchBook();
+      }
+    });
+
+  });
+
+});
